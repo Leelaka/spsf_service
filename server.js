aaec8346475d7f1cf630dfb3c0d7d4e76673a6e3
ruwanganath ramanayake <ruwanganath@Samadi.localdomain>
2021-04-10 02:34:48 +1000

    spsf_service cont delivery test

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 var express = require('express'),
     app = express();
 
+const req = require('request');
+
 var port = process.env.PORT || 8080;   
 
 app.use(express.static(__dirname +'/public'));
 
-app.get('/',function(req,res){
-   res.send('spsf_service')
+app.get('/',function(request,response){
+   response.send('spsf_service')
 })
+
+app.get('/name',function(request,response){
+    response.send('name - spsf_service')
+ })
 
 app.listen(port);
 console.log('Server listening on : '+port);
