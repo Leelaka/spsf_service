2ad2790dbbb921305930b4a5ed835128901e5701
ruwanganath <46487874+ruwanganath@users.noreply.github.com>
2021-04-24 18:44:56 +1000

    Update server.js

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const req = require('request');
 
 app = express();
 var port = process.env.PORT || 8080;   
-//var spsfUrl = 'https://spsfwebfront.mybluemix.net';
-//var spsfDataanalysisUrl = 'https://spsfdataanalysis.mybluemix.net';
-var spsfUrl = 'http://localhost:3000';
-var spsfDataanalysisUrl = 'http://localhost:8081';
+var spsfUrl = 'https://spsfwebfront.mybluemix.net';
+var spsfDataanalysisUrl = 'https://spsfdataanalysis.mybluemix.net';
+//var spsfUrl = 'http://localhost:3000';
+//var spsfDataanalysisUrl = 'http://localhost:8081';
 var parkingData;
 
 const uri = "mongodb+srv://sit725:sit725@sit725.gwuvj.mongodb.net/spsf?retryWrites=true&w=majority";
