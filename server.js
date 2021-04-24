3ed9fe9b50bb2efa8b55cf4bcb7d6a51b8d28514
ruwanganath ramanayake <ruwanganath@Samadi.localdomain>
2021-04-24 15:03:53 +1000

    geting data for parking map

--- server.js
+++ server.js
@@ -1,19 +1,81 @@
-var express = require('express'),
-    app = express();
-
+const MongoClient = require('mongodb').MongoClient;
+var express = require('express')
 const req = require('request');
 
+app = express();
 var port = process.env.PORT || 8080;   
+//var spsfUrl = 'https://spsfwebfront.mybluemix.net';
+//var spsfDataanalysisUrl = 'https://spsfdataanalysis.mybluemix.net';
+var spsfUrl = 'http://localhost:3000';
+var spsfDataanalysisUrl = 'http://localhost:8081';
+var parkingData;
+
+const uri = "mongodb+srv://sit725:sit725@sit725.gwuvj.mongodb.net/spsf?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true});
 
 app.use(express.static(__dirname +'/public'));
+//use express boady parser to get view data
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/',function(request,response){
-   response.send('spsf_service')
+//db collections
+client.connect(err => {
+  collectionUsers = client.db("spsf").collection("user");
+});
+
+//registration process
+app.get('/register', function (request,response) {
+
+    let username = request.query.username
+    let email = request.query.email
+    let password = request.query.password
+    let confirmPassword = request.query.confirmpassword
+
+    if (password === confirmPassword) {
+  
+      collectionUsers.findOne({ email: email }, function (err, result) {
+        if (err) throw err;
+  
+        if (result === null) {
+          collectionUsers.insertOne({ username: username, email: email , password: password });
+          response.json({registration:'true', message:''})
+        } else {
+          response.json({registration:'false', message:'Error: username is already exist.'}) 
+        }
+      });  
+    } else {
+      response.json({registration:'false', message:'Error: password mismatched.'})
+    }
+})
+  
+//Authenticate user process
+app.get('/authenticate', function (request, response) {
+
+  let username = request.query.username
+  let password = request.query.password
+
+  collectionUsers.findOne({ username: username, password: password }, function (err, result) {
+    if (err) throw err;
+    if (result === null){
+      response.json({authorisation:'false', message:'Error:invalid credentials.'})
+    }else{
+      response.json({authorisation:'true', message:''})
+    }
+  })
+})
+  
+//Request all parking data from data analysis service
+app.get('/requestAllParkingData',function (request,response){
+    reqObject = spsfDataanalysisUrl+"/generateParkingData";
+    req(reqObject,(err,result,body)=> {
+        if(err){
+            return console.log(err);
+        }  
+        parkingData = result.body
+              
+    });
+     response.send(parkingData)
 })
 
-app.get('/name',function(request,response){
-    response.send('name - spsf_service')
- })
 
 app.listen(port);
 console.log('Server listening on : '+port);
