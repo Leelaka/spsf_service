e0c2743ddf798d1995178db087ae3df6175f36a6
Adassuriyage Don Leelaka Indira <liad@deakin.edu.au>
2021-05-13 22:27:00 +1000

    added hashing

--- server.js
+++ server.js
@@ -1,17 +1,22 @@
 const MongoClient = require('mongodb').MongoClient;
 var express = require('express')
 const req = require('request');
+const session = require('express-session'); //session
+const MongoDBSession = require('connect-mongodb-session')(session)
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 app = express();
 var port = process.env.PORT || 8080;   
-var spsfUrl = 'https://spsfwebfront.mybluemix.net';
-var spsfDataanalysisUrl = 'https://spsfdataanalysis.us-south.cf.appdomain.cloud';
-//var spsfUrl = 'http://localhost:3000';
-//var spsfDataanalysisUrl = 'http://localhost:8081';
+//var spsfUrl = 'https://spsfwebfront.mybluemix.net';
+//var spsfDataanalysisUrl = 'https://spsfdataanalysis.us-south.cf.appdomain.cloud';
+var spsfUrl = 'http://localhost:3000';
+var spsfDataanalysisUrl = 'http://localhost:8081';
 var parkingData;
 
 const uri = "mongodb+srv://sit725:sit725@sit725.gwuvj.mongodb.net/spsf?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true});
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true})
+
 
 app.use(express.static(__dirname +'/public'));
 
@@ -21,15 +26,32 @@ app.use(express.urlencoded({ extended: true }));
 //db collections
 client.connect(err => {
   collectionUsers = client.db("spsf").collection("user");
+  if(!err) console.log('user collection connected'); //res db connected 
 });
 
+const store = new MongoDBSession({
+  uri: uri,
+  collection: 'userSessions',
+});
+
+app.use(
+  session({
+    secret: "key that will sign cookies",
+    resave: false,
+    saveUninitialized: false,
+    store: store,
+  })
+);
+
 //registration process
-app.get('/register', function (request,response) {
+app.get('/register', async (request,response) => {
 
     let username = request.query.username
     let email = request.query.email
     let password = request.query.password
     let confirmPassword = request.query.confirmpassword
+
+    const hashPassword = await bcrypt.hash(password, saltRounds);
 
     if (password === confirmPassword) {
   
@@ -37,7 +59,7 @@ app.get('/register', function (request,response) {
         if (err) throw err;
   
         if (result === null) {
-          collectionUsers.insertOne({ username: username, email: email , password: password });
+          collectionUsers.insertOne({ username: username, email: email , password: hashPassword });
           response.json({registration:'true', message:''})
         } else {
           response.json({registration:'false', message:'Error: username is already exist.'}) 
@@ -46,23 +68,41 @@ app.get('/register', function (request,response) {
     } else {
       response.json({registration:'false', message:'Error: password mismatched.'})
     }
-})
+});
   
 //Authenticate user process
-app.get('/authenticate', function (request, response) {
+app.get('/authenticate', async (request, response) => {
 
   let username = request.query.username
   let password = request.query.password
 
-  collectionUsers.findOne({ username: username, password: password }, function (err, result) {
-    if (err) throw err;
-    if (result === null){
+  const user = await collectionUsers.findOne({username : username});
+
+  if(!username)
+  {
+      throw err;
+  }
+  
+  const comparePassword = await bcrypt.compare(password, user.password);
+
+  if (!comparePassword){
       response.json({authorisation:'false', message:'Error: invalid credentials.'})
-    }else{
+  }
+  else
+  {
       response.json({authorisation:'true', message:''})
-    }
-  })
-})
+  }
+  
+
+  // collectionUsers.findOne({ username: username, password: password }, function (err, result) {
+  //   if (err) throw err;
+  //   if (result === null){
+  //     response.json({authorisation:'false', message:'Error: invalid credentials.'})
+  //   }else{
+  //     response.json({authorisation:'true', message:''})
+  //   }
+  // })
+});
   
 //Request all parking data from data analysis service
 app.get('/requestAllParkingData',function (request,response){
