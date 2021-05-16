1fcf9b419589aca8ddb686801de3a5cfad1533ac
Adassuriyage Don Leelaka Indira <liad@deakin.edu.au>
2021-05-16 12:47:09 +1000

    hash pass login and reg

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const MongoClient = require('mongodb').MongoClient;
-var express = require('express')
+var express = require('express');
 const req = require('request');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 app = express();
 var port = process.env.PORT || 8080;   
-var spsfUrl = 'https://spsfwebfront.mybluemix.net';
-var spsfDataanalysisUrl = 'https://spsfdataanalysis.us-south.cf.appdomain.cloud';
-//var spsfUrl = 'http://localhost:3000';
-//var spsfDataanalysisUrl = 'http://localhost:8081';
+//var spsfUrl = 'https://spsfwebfront.mybluemix.net';
+//var spsfDataanalysisUrl = 'https://spsfdataanalysis.us-south.cf.appdomain.cloud';
+var spsfUrl = 'http://localhost:3000';
+var spsfDataanalysisUrl = 'http://localhost:8081';
 var parkingData;
 
 const uri = "mongodb+srv://sit725:sit725@sit725.gwuvj.mongodb.net/spsf?retryWrites=true&w=majority";
@@ -24,46 +26,58 @@ client.connect(err => {
 });
 
 //registration process
-app.get('/register', function (request,response) {
+  
+app.get('/register', async (request,response) => {
 
-    let username = request.query.username
-    let email = request.query.email
-    let password = request.query.password
-    let confirmPassword = request.query.confirmpassword
+  let username = request.query.username
+  let email = request.query.email
+  let password = request.query.password
+  let confirmPassword = request.query.confirmpassword
 
-    if (password === confirmPassword) {
-  
-      collectionUsers.findOne({ email: email }, function (err, result) {
-        if (err) throw err;
-  
-        if (result === null) {
-          collectionUsers.insertOne({ username: username, email: email , password: password });
-          response.json({registration:'true', message:''})
-        } else {
-          response.json({registration:'false', message:'Error: username is already exist.'}) 
-        }
-      });  
-    } else {
-      response.json({registration:'false', message:'Error: password mismatched.'})
-    }
-})
-  
+  const hashPassword = await bcrypt.hash(password, saltRounds);
+
+  if (password === confirmPassword) {
+
+    collectionUsers.findOne({ email: email }, function (err, result) {
+      if (err) throw err;
+
+      if (result === null) {
+        collectionUsers.insertOne({ username: username, email: email , password: hashPassword });
+        response.json({registration:'true', message:''})
+      } else {
+        response.json({registration:'false', message:'Error: username is already exist.'}) 
+      }
+    });  
+  } else {
+    response.json({registration:'false', message:'Error: password mismatched.'})
+  }
+});
+
 //Authenticate user process
-app.get('/authenticate', function (request, response) {
+app.get('/authenticate', async (request, response) => {
 
   let username = request.query.username
   let password = request.query.password
 
-  collectionUsers.findOne({ username: username, password: password }, function (err, result) {
-    if (err) throw err;
-    if (result === null){
-      response.json({authorisation:'false', message:'Error: invalid credentials.'})
-    }else{
-      response.json({authorisation:'true', message:''})
-    }
-  })
-})
+  const user = await collectionUsers.findOne({username : username});
+
+  if(!username)
+  {
+      throw err;
+  }
   
+  const comparePassword = await bcrypt.compare(password, user.password);
+
+  if (!comparePassword){
+      response.json({authorisation:'false', message:'Error: invalid credentials.'})
+  }
+  else
+  {
+      response.json({authorisation:'true', message:''})
+  }
+  
+});
+
 //Request all parking data from data analysis service
 app.get('/requestAllParkingData',function (request,response){
     reqObject = spsfDataanalysisUrl+"/generateParkingData";
